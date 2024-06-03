f8bf23e91cc5ff4fac49d76481dfc7b5ea982f73
mrpye <stretchmaterp@googlemail.com>
2024-06-03 10:00:37 +0100

    saved a copy

---
+++ go.bat
@@ -0,0 +1,4 @@
+cd C:\projects\Dome\focus_converters\focus_converter_base
+C:/Users/mrpye/AppData/Roaming/Python/Scripts/poetry build
+pip uninstall ./dist/focus_converter-0.7.1.dev9-py3-none-any.whl  
+pip install ./dist/focus_converter-0.7.1.dev9-py3-none-any.whl
